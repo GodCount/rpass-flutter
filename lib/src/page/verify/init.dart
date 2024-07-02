4a01b65c3ae27d0ec0ef6779e56afd6a18ab3f6b
wenyan <do_yzr@163.com>
2024-07-02 21:31:58 +0800

    实现初始化密码ui

--- lib/src/page/verify/init.dart
+++ lib/src/page/verify/init.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../component/component.dart';
+// import '../../component/component.dart';
 import '../../store/verify/contrller.dart';
 
 class InitPassword extends StatefulWidget {
@@ -15,57 +15,82 @@ class InitPassword extends StatefulWidget {
 }
 
 class InitPasswordState extends State<InitPassword> {
-  TextEditingController _pwd1Controller = TextEditingController();
-  TextEditingController _pwd2Controller = TextEditingController();
-  GlobalKey _formKey = GlobalKey<FormState>();
+  final TextEditingController _pwd1Controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(24.0),
+        padding: const EdgeInsets.all(24),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
           children: [
             const Text(
               "H1 HHHHH",
               style: TextStyle(fontSize: 18),
             ),
+            const SizedBox(
+              height: 12,
+            ),
             const Text(
                 "Lorem qui enim officia elit veniam elit velit sint in."),
+            const SizedBox(
+              height: 24,
+            ),
             Form(
               key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _pwd1Controller,
-                    decoration: const InputDecoration(
-                        labelText: "init password",
-                        border: OutlineInputBorder()),
-                    validator: (value) {
-                      return value == null || value.trim().isEmpty
-                          ? "be not empty"
-                          : value.length > 3
-                              ? null
-                              : "must length > 3";
-                    },
-                  ),
-                  TextFormField(
-                    controller: _pwd2Controller,
-                    decoration: const InputDecoration(
-                        labelText: "init password",
-                        border: OutlineInputBorder()),
-                    validator: (value) {
-                      return value == _pwd1Controller.value
-                          ? null
-                          : "must equal";
-                    },
-                  )
-                ],
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: _pwd1Controller,
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.next,
+                      decoration: const InputDecoration(
+                          labelText: "init password",
+                          border: OutlineInputBorder()),
+                      validator: (value) {
+                        return value == null || value.trim().isEmpty
+                            ? "be not empty"
+                            : value.length > 3
+                                ? null
+                                : "must length > 3";
+                      },
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                      decoration: const InputDecoration(
+                          labelText: "confirm init password",
+                          border: OutlineInputBorder()),
+                      validator: (value) {
+                        return value == _pwd1Controller.value.text
+                            ? null
+                            : "must equal";
+                      },
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    IconButton.filledTonal(
+                        onPressed: () {
+                          // _formKey.currentState?.validate();
+                        },
+                        iconSize: 48,
+                        icon: const Icon(Icons.keyboard_arrow_right_rounded))
+                  ],
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
