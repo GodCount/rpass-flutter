ecd50ba278d4bbe29a76436f3cb6e2ddbcbc2169
GodCount <do_yzr@163.com>
2026-08-14 16:51:14 +0800

    Squashed 'packages/keepass_core/rust/keepass/' content from commit 030d624
    git-subtree-dir: packages/keepass_core/rust/keepass
    git-subtree-split: 030d6242a8c9b34e82c088f8a4c2e86e3773887a

---
+++ tests/file_read_tests.rs
@@ -0,0 +1,530 @@
+//! Integration tests for various database examples
+
+#[allow(
+    missing_docs,
+    clippy::indexing_slicing,
+    clippy::expect_used,
+    clippy::unwrap_used
+)]
+mod file_read_tests {
+    use keepass::{
+        config::DatabaseVersion,
+        db::{CustomDataValue, Database, DatabaseOpenError, GroupRef},
+        DatabaseKey,
+    };
+
+    use uuid::uuid;
+
+    use std::{fs::File, path::Path};
+
+    fn explore(parent: GroupRef<'_>) -> (usize, usize) {
+        let mut total_entries = 0;
+        let mut total_groups = 1;
+        for entry in parent.entries() {
+            let title = entry.get_title().unwrap_or("(no title)");
+            let user = entry.get_username().unwrap_or("(no user)");
+            let pass = entry.get_password().unwrap_or("(no password)");
+            println!("Entry '{0}': '{1}' : '{2}'", title, user, pass);
+            total_entries += 1;
+        }
+
+        for group in parent.groups() {
+            println!("Saw group '{0}'", group.name);
+            let (ee, eg) = explore(group);
+
+            total_entries += ee;
+            total_groups += eg;
+        }
+
+        (total_entries, total_groups)
+    }
+
+    #[test]
+    fn open_kdbx3_with_password() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_with_password.kdbx");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB3(1));
+        assert_eq!(db.root().name, "sample");
+        assert_eq!(db.root().groups().count(), 3);
+        assert_eq!(db.root().entries().count(), 2);
+
+        let (total_entries, total_groups) = explore(db.root());
+        assert_eq!(total_groups, 5);
+        assert_eq!(total_entries, 6);
+
+        println!("{:?}", db);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx3_with_keyfile() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_with_keyfile.kdbx");
+        let kf_path = Path::new("tests/resources/test_key.key");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_keyfile(&mut File::open(kf_path)?)?,
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB3(1));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        let (total_entries, total_groups) = explore(db.root());
+        assert_eq!(total_groups, 1);
+        assert_eq!(total_entries, 1);
+
+        println!("{:?}", db);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx3_with_keyfile_xml() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_with_keyfile_xml.kdbx");
+        let kf_path = Path::new("tests/resources/test_key_xml.key");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_keyfile(&mut File::open(kf_path)?)?,
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB3(1));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 2);
+        assert_eq!(db.root().entries().count(), 2);
+
+        let (total_entries, total_groups) = explore(db.root());
+        assert_eq!(total_groups, 5);
+        assert_eq!(total_entries, 6);
+
+        println!("{:?}", db);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_kdf_argon2_cipher_aes() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_argon2.kdbx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_kdf_argon2id_cipher_aes() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_argon2id.kdbx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_kdf_aes_cipher_aes() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_aes.kdbx");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(1));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_kdf_argon2_cipher_twofish() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_argon2_twofish.kdbx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_kdf_argon2_cipher_chacha20() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_argon2_chacha20.kdbx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_kdf_argon2id_cipher_twofish() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_argon2id_twofish.kdbx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_kdf_argon2id_cipher_chacha20() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_argon2id_chacha20.kdbx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_keyfile() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_keyfile.kdbx");
+        let kf_path = Path::new("tests/resources/test_key.key");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_keyfile(&mut File::open(kf_path)?)?,
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_keyfile_v2() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_keyfile_v2.kdbx");
+        let kf_path = Path::new("tests/resources/test_db_kdbx4_with_keyfile_v2.keyx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new()
+                .with_password("demopass")
+                .with_keyfile(&mut File::open(kf_path)?)?,
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        Ok(())
+    }
+
+    /// keyfile with tabs in the keyfile content (#284)
+    #[test]
+    fn open_kdbx4_with_keyfile_v2_alt() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_keyfile_v2_alt.kdbx");
+        let kf_path = Path::new("tests/resources/test_db_kdbx4_with_keyfile_v2_alt.keyx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new()
+                .with_password("demopass")
+                .with_keyfile(&mut File::open(kf_path)?)?,
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(0));
+        assert_eq!(db.root().name, "testdb02");
+        assert_eq!(db.root().groups().count(), 6);
+        assert_eq!(db.root().entries().count(), 2);
+
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic(expected = r#"InvalidKDBXIdentifier"#)]
+    fn open_broken_random_data() {
+        let path = Path::new("tests/resources/broken_random_data.kdbx");
+        Database::open(
+            &mut File::open(path).unwrap(),
+            DatabaseKey::new().with_password(""),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = r#"InvalidKDBXVersion"#)]
+    fn open_broken_kdbx_version() {
+        let path = Path::new("tests/resources/broken_kdbx_version.kdbx");
+        Database::open(
+            &mut File::open(path).unwrap(),
+            DatabaseKey::new().with_password(""),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn open_kdb_with_password() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdb_with_password.kdb");
+        let db = Database::open(&mut File::open(path)?, DatabaseKey::new().with_password("foobar"))?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB(2));
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 3);
+
+        let (total_entries, total_groups) = explore(db.root());
+        assert_eq!(total_groups, 12);
+        assert_eq!(total_entries, 5);
+
+        println!("{:?}", db);
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdb_with_larger_than_1mb_file_does_not_crash() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdb3_with_file_larger_1mb.kdbx");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+        assert_eq!(db.config.version, DatabaseVersion::KDB3(1));
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 1);
+
+        let (total_entries, total_groups) = explore(db.root());
+        assert_eq!(total_groups, 1);
+        assert_eq!(total_entries, 1);
+
+        println!("{:?}", db);
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_with_password_deleted_entry() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_deleted_entry.kdbx");
+
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(
+            db.meta.recyclebin_uuid,
+            Some(uuid!("563171fe-6598-42dc-8003-f98dde32e872"))
+        );
+
+        let recycle_bin = db.recycle_bin().unwrap();
+        assert_eq!(recycle_bin.name, "Recycle Bin");
+
+        Ok(())
+    }
+
+    #[test]
+    #[cfg(feature = "challenge_response")]
+    fn open_kdbx4_with_challenge_response_key() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_with_challenge_response_key.kdbx");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new()
+                .with_password("demopass")
+                .with_challenge_response_key(keepass::ChallengeResponseKey::LocalChallenge(
+                    "0102030405060708090a0b0c0d0e0f1011121314".to_string(),
+                )),
+        )?;
+
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 2);
+        Ok(())
+    }
+
+    #[test]
+    #[ignore]
+    #[cfg(feature = "challenge_response")]
+    fn open_kdbx4_with_yubikey_challenge_response_key() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_with_challenge_response_key.kdbx");
+        let yubikey = keepass::ChallengeResponseKey::get_yubikey(None).expect("Failed to get Yubikey");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new()
+                .with_password("demopass")
+                .with_challenge_response_key(keepass::ChallengeResponseKey::YubikeyChallenge(
+                    yubikey,
+                    "2".to_string(),
+                )),
+        )?;
+
+        assert_eq!(db.root().name, "Root");
+        assert_eq!(db.root().groups().count(), 0);
+        assert_eq!(db.root().entries().count(), 2);
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx41_with_password() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_kdbx41_with_password_aes.kdbx");
+        let db = Database::open(
+            &mut File::open(path)?,
+            DatabaseKey::new().with_password("demopass"),
+        )?;
+
+        println!("{:?} DB Opened", db);
+
+        assert_eq!(db.config.version, DatabaseVersion::KDB4(1));
+
+        assert!(db
+            .meta
+            .custom_data
+            .get("KeePassRPC.Config")
+            .unwrap()
+            .last_modification_time
+            .is_some());
+
+        assert_eq!(db.root().name, "Database");
+        assert_eq!(db.root().groups().count(), 2);
+        assert_eq!(db.root().entries().count(), 4);
+
+        let root = db.root();
+        let group = root.group_by_name("Group with tags").unwrap();
+        assert_eq!(
+            group.tags,
+            vec!["a".to_string(), "b".to_string(), "c".to_string()]
+        );
+
+        let group_id = group.id();
+
+        assert!(group.previous_parent().is_none());
+
+        let entry = root.entry_by_name("entry with no quality check").unwrap();
+        assert!(!entry.quality_check);
+
+        let entry = root.entry_by_name("entry with named custom icon").unwrap();
+        assert!(entry.quality_check);
+
+        let icon = entry.custom_icon().unwrap();
+        assert_eq!(icon.name, Some("Egg".to_string()));
+        assert!(icon.last_modification_time.is_some());
+
+        let entry = root.entry_by_name("entry with custom data").unwrap();
+        let cd = entry.custom_data.get("KPRPC JSON").unwrap();
+        assert_eq!(
+            cd.value.as_ref().unwrap(),
+            &CustomDataValue::String("{\"version\":2,\"altUrls\":[\"https://example.com\",\"http://example.com\"],\"blockedUrls\":[],\"regExBlockedUrls\":[],\"regExUrls\":[],\"authenticationMethods\":[\"password\"],\"matcherConfigs\":[{\"matcherType\":\"Url\",\"urlMatchMethod\":\"Domain\"}],\"fields\":[{\"uuid\":\"nkd4kOJMJ0qPOJaAjoTXcw==\",\"valuePath\":\"UserName\",\"page\":1,\"type\":\"Text\",\"matcherConfigs\":[{\"matcherType\":\"UsernameDefaultHeuristic\"}]},{\"uuid\":\"7xtQBZ2+wEizAxNe5rYKzg==\",\"valuePath\":\"Password\",\"page\":1,\"type\":\"Password\",\"matcherConfigs\":[{\"matcherType\":\"PasswordDefaultHeuristic\"}]}]}".to_string())
+        );
+
+        let entry = root.entry_by_name("entry that was moved").unwrap();
+        assert_eq!(entry.previous_parent().unwrap().id(), group_id);
+
+        let moved_group = root.group_by_name("Group that was moved").unwrap();
+        assert_eq!(moved_group.previous_parent().unwrap().id(), group_id);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_version() -> Result<(), DatabaseOpenError> {
+        let path = Path::new("tests/resources/test_db_with_password.kdbx");
+        let version = Database::get_version(&mut File::open(path)?)?;
+        assert_eq!(version.to_string(), "KDBX3.1");
+
+        let path = Path::new("tests/resources/test_db_kdbx4_with_password_argon2.kdbx");
+        let version = Database::get_version(&mut File::open(path)?)?;
+        assert_eq!(version.to_string(), "KDBX4.0");
+
+        Ok(())
+    }
+
+    #[test]
+    fn open_kdbx4_fuzzing() -> Result<(), DatabaseOpenError> {
+        use std::io::Read;
+        let path = Path::new("tests/resources/test_db_few_rounds.kdbx");
+        let file_len = path.metadata()?.len() as usize;
+
+        let mut file_as_vec = Vec::new();
+        File::open(path)?.read_to_end(&mut file_as_vec)?;
+
+        for one_len in 0..=file_len {
+            print!("Trying length: {}", one_len);
+            let current_slice = &file_as_vec[..one_len];
+            let res = Database::parse(current_slice, DatabaseKey::new().with_password("demopass"));
+            match res {
+                Ok(db) => {
+                    println!(" - DB Opened");
+
+                    assert_eq!(db.root().name, "Root");
+                    assert_eq!(db.root().entries().count(), 1);
+                }
+                Err(_) => {
+                    println!(" - failed with error");
+                    continue;
+                    // we don't care about error, it's normal, we just check for panic
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
