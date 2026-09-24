13eb553777c26f36e18730fb433fc12b4d5961c5
GodCount <do_yzr@163.com>
2026-09-24 17:31:00 +0800

    使用 atomic_write 的方式保存文件

--- packages/keepass_core/rust/bridge/src/api/kdbx.rs
+++ packages/keepass_core/rust/bridge/src/api/kdbx.rs
@@ -45,7 +45,7 @@ use utils_proc_macro::frb_string_constant;
 use zeroize::Zeroize;
 
 use crate::api::kdbx::KdbxAction::UpdateSyncEntry;
-use crate::api::utils::{contains_domain, random_bytes, simple_to_domain, transform_xor};
+use crate::api::utils::{atomic_write, contains_domain, random_bytes, simple_to_domain, transform_xor};
 use crate::frb_generated::FLUTTER_RUST_BRIDGE_HANDLER;
 
 frb_string_constant! {
@@ -251,10 +251,7 @@ impl Kdbx {
 
         let bytes = self.save()?;
 
-        let mut file = File::create(file_path)?;
-
-        let _ = file.write(&bytes)?;
-        file.flush()?;
+        atomic_write(file_path, &bytes)?;
 
         self.emit(KdbxEvent::Saved);
 
